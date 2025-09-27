cb7b66d4d185a068a6d904035d2a9ecd902e2fcd
leonardo <boleo88@gmail.com>
2025-09-27 21:26:25 +0200

    add get_lennard_jones and get_network_params

---
+++ R/globals.R
@@ -0,0 +1,5 @@
+utils::globalVariables(c(
+  ".", "flush.console", "lj_res", "r_avg", "resid_i", "resid_j", "vertex",
+  "unique_residues", "group_i", "group_j", "ch_i", "ch_j",
+  "all_res", "g1_res", "g2_res"
+))
